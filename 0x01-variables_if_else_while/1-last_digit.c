6abca4e1d6df1051042a069e026ce7a04bf93eb2
danny-kings <dannyafolabs@gmail.com>
2022-09-10 16:33:38 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ else if (1 == 0)
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	printf("Last digit of %d is %d and is 0 and is 0\n", n, l);
 }
 return (0);
 }
