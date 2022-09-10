61b6a5025a0b9269baa1c78b16a8f3ba6c4ed965
danny-kings <dannyafolabs@gmail.com>
2022-09-10 13:37:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>                                                            
-#include <unistd.h>                                                           
-/**                                                                           
-* main - entry point                                                         
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - entry point
 *
-* Description: prints and that piece..
+*Description: prints and that piece..
 *Return: 1 if sucessful
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);   
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
