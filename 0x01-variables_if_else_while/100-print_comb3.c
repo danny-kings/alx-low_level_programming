c06ed18307143e8cbfd98d720c17d88938ec2281
danny-kings <dannyafolabs@gmail.com>
2022-09-10 18:28:59 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,10 @@ int main(void)
 	{
 		f_d = (c / 10 + '0');
 		l_d = (c % 10 + '0');
-		
 		if (f_d < l_d)
 		{
 			putchar(f_d);
 			putchar(l_d);
-			
 			if (c != 89)
 			{
 				putchar(',');
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,19 +15,17 @@ int main(void)
 		f_d = (c / 100 + '0');
 		m_d = (c / 10 % 10 + '0');
 		l_d = (c % 10 + '0');
-		
 		if ((f_d < m_d) && (m_d < l_d))
 		{
 			putchar(f_d);
 			putchar(m_d);
 			putchar(l_d);
-			
 			if (c != 789)
 			{
 				putchar(',');
 				putchar(' ');
 			}
-		}	
+		}
 		c++;
 }
 putchar('\n');
