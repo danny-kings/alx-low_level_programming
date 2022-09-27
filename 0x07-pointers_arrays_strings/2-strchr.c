c79ba4456e50ff9ae528c5be6c20d6a0f70bf771
danny-kings <dannyafolabs@gmail.com>
2022-09-26 20:35:39 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,20 +3,19 @@
 /**
  * _strchr - locate a character in a string
  * @s: a string
- * @c: the character to locate
+ * @c: character to check for
  *
- * Return: a pointer to the character if fould, otherwise NULL
+ * Return: pointer to spot in s with c or null
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int length, i;
+	int i;
 
-	for (length = 0; s[length]; length++)
-		continue;
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s +i);
+	}
 
-
-	for (i = 0; i <= length; i++)
-		if (s[i] == c)
-			return (s + i);
-	return (NULL);
+	return (0);
 }
